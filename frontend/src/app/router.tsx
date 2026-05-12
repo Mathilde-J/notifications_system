fa9dcd2697365bc03fc32116dfcd3d8079ffb22e
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2026-05-12 17:32:15 +0200

    [add] setup projects, react, router, tanstackquery (#15)
    * [add] setup projects, react, router, tanstackquery
    
    * [add] added testing packages

---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../shared/layout/MainLayout";
+import MessagesPage from "../pages/MessagesPage";
+import NewMessagePage from "../pages/NewMessagePage";
+
+export const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <MessagesPage /> },
+      { path: "/messages/new", element: <NewMessagePage /> },
+    ]
+  }
+])
---
+++ frontend/src/pages/NewMessagePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewMessagePage: React.FC = () => {
+  return <div>{/* NewMessagePage content */}</div>;
+};
+
+export default NewMessagePage;
