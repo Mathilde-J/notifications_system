e8507bdb40b497e4ceedb04a2910de3c2be6501a
Mathilde <mthldjanssen@gmail.com>
2026-05-14 11:35:11 +0200

    [fix] clean backend files

--- backend/interfaces/observer/observable.ts
+++
@@ -1,9 +0,0 @@
-import type { EventResponse } from "../../types/log";
-import type { Observer } from "./observer";
-
-export interface Observable<T> {
-  subscribers: Observer<T>[];
-  subscribe(observer: Observer<T>): void;
-  unsubscribe(observer: Observer<T>): void;
-  notifyObserver(data: T, status: EventResponse): void;
-}
