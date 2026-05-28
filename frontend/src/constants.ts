4237293612d506b00d8211b5c2ff7d48d127a446
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2026-05-28 14:42:28 +0200

    [add:Link] added a href github (#19)

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,3 @@
 export const urlApiBase = "http://localhost:3000/api/";
 export const urlMessages = "messages"
+export const githubLink = "https://github.com/Mathilde-J/notifications_system"
--- frontend/src/shared/layout/MainLayout.tsx
+++ frontend/src/shared/layout/MainLayout.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router";
 import style from "./style.module.css";
-import { ButtonType } from "../components/constant";
-import { AppButton } from "../components/appButton/AppButton";
+import { githubLink } from "../../constants";
 
 export default function MainLayout() {
   return (
     <main className={style.main_layout}>
       <header className={style.main_layout_header}>
-        <AppButton
-          title={"Accéder au github"}
-          buttonType={ButtonType.SECONDARY}
-        />
+        <a href={githubLink} rel="noopener noreferrer">
+          Accéder à Github
+        </a>
       </header>
       <Outlet />
     </main>
